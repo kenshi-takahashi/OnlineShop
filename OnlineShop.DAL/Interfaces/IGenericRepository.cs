fab1e1f26dd09ef1c69e64e748d665043a6eabc3
Kenshi-Takahashi <suslikgory12345555@gmail.com>
2024-06-21 22:59:22 +0300

    replace intarfaces for repositories from BLL to DAL

--- OnlineShop.DAL/Interfaces/IGenericRepository.cs
+++ OnlineShop.DAL/Interfaces/IGenericRepository.cs
@@ -1,14 +1,14 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace MyOnlineShop.DAL.Interfaces
 {
-	public interface IGenericRepository<T> where T : class
-	{
-		Task<T> GetByIdAsync(int id);
-		Task<IEnumerable<T>> GetAllAsync();
-		Task AddAsync(T entity);
-		Task UpdateAsync(T entity);
-		Task DeleteAsync(int id);
-	}
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
 }
